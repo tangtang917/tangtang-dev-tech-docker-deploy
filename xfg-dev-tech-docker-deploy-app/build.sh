803687302c39978b67a67779c8079967db65182f
fuzhengwei <184172133@qq.com>
2025-12-22 07:38:37 +0800

    feat: init project

--- xfg-dev-tech-docker-deploy-app/build.sh
+++ xfg-dev-tech-docker-deploy-app/build.sh
@@ -1,7 +1,7 @@
 # 普通镜像构建，随系统版本构建 amd/arm
-docker build --platform linux/amd64 --load -t fuzhengwei/xfg-dev-tech-docker-deploy-app:1.2-SNAPSHOT -f ./Dockerfile .
+docker build -t fuzhengwei/xfg-dev-tech-docker-deploy-app:1.4-SNAPSHOT -f ./Dockerfile .
 
-# docker build -t fuzhengwei/xfg-dev-tech-docker-deploy-app:1.2-SNAPSHOT -f ./Dockerfile .
+#docker build --platform linux/amd64 --load -t fuzhengwei/xfg-dev-tech-docker-deploy-app:1.4-SNAPSHOT -f ./Dockerfile .
 
 # 兼容 amd、arm 构建镜像
 #docker buildx build --load --platform linux/amd64,linux/arm64 -t system/xfg-dev-tech-docker-deploy-app:1.1-SNAPSHOT -f ./Dockerfile .
